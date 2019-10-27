f2b265d92f07d8b8ef5581e11a790e19993f28b9
Thiago Giammattey <giammattey@gmail.com>
2019-10-27 00:03:15 -0300

    handling error

--- pages/07-random-ron/js/main.js
+++ pages/07-random-ron/js/main.js
@@ -2,19 +2,33 @@
   "use strict";
 
   const btn = document.querySelector("button");
-  const quote = document.querySelector("blockquote");
+  const blockquote = document.querySelector("blockquote");
+  const endPoint = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
-  function getQuote() {
-    fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+  function fetchQuote() {
+    blockquote.setAttribute("data-error", "false");
+    blockquote.textContent = "LOADING...";
+
+    fetch(endPoint)
       .then(function(response) {
-        return response.json();
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(response);
+        }
       })
       .then(function(data) {
-        quote.textContent = data;
+        blockquote.textContent = data;
+      })
+      .catch(function(error) {
+        console.warn("Error:", error.status, error.statusText);
+        blockquote.setAttribute("data-error", "true");
+        blockquote.textContent =
+          "Sorry, something went wrong. Please try again later.";
       });
   }
 
-  getQuote();
+  fetchQuote();
 
-  btn.addEventListener("click", getQuote, false);
+  btn.addEventListener("click", fetchQuote, false);
 })();
