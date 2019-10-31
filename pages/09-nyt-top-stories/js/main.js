32a4aa8be92cbdf4b95d84fc44bd348d9d7b0acf
Thiago Giammattey <giammattey@gmail.com>
2019-10-31 11:46:58 -0300

    cleaning up

--- pages/09-nyt-top-stories/js/main.js
+++ pages/09-nyt-top-stories/js/main.js
@@ -6,31 +6,31 @@
   const endpoint = `https://api.nytimes.com/svc/topstories/v2/science.json?api-key=${apiKey}`;
 
   const render = function(articles) {
-    app.innerHTML = articles
-      .map(article => {
-        return `
+    return articles
+      .map(
+        article => `
         <article>
-        <h2><a href="${article.url}">${article.title}</a></h2>
-        <p>${article.byline}</p>
-        <p>${article.abstract}</p>
-        <img src="${decodeURIComponent(
-          article.multimedia[0].url
-        )}" alt="${decodeURIComponent(article.multimedia[0].caption)}"/>
-      </article>
-      `;
-      })
+          <h2><a href="${article.url}">${article.title}</a></h2>
+            <p>${article.byline}</p>
+            <p>${article.abstract}</p>
+            <img src="${article.multimedia[3].url}" alt="${article.multimedia[3].caption}"/>
+        </article>
+      `
+      )
       .join("");
   };
 
   fetch(endpoint)
     .then(function(response) {
-      if (response.ok) {
-        return response.json();
-      }
+      if (response.ok) return response.json();
       return Promise.reject(response);
     })
     .then(function(data) {
-      render(data.results);
+      return render(data.results);
+    })
+    .then(function(htmlString) {
+      app.innerHTML = htmlString;
+      return app;
     })
     .catch(function(error) {
       console.log("Something went wrong:", error);
