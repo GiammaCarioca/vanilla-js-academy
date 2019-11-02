c75d16b26a3c99de5a578f9dcb64a5e7c911935a
Thiago Giammattey <giammattey@gmail.com>
2019-11-02 07:20:39 -0300

    just some renaming

--- pages/10-nyt-multiple-categories/js/main.js
+++ pages/10-nyt-multiple-categories/js/main.js
@@ -3,32 +3,33 @@
 
 	const app = document.querySelector('#app');
 	const apiKey = 'cGKSCRZuXhcpv5wX59icpRRFGAqG4Mg8';
-	const categories = ['arts', 'fashion', 'movies'];
+	const sections = ['arts', 'fashion', 'movies'];
+	const numOfArticles = 5;
 
 	function getJSON(response) {
 		if (response.ok) return response.json();
 		return Promise.reject(response);
 	}
 
-	function getTopStories(articles, num) {
-		const topStories = articles.slice(0, num);
+	function getTopStories(articles, numOfArticles) {
+		const topStories = articles.slice(0, numOfArticles);
 		return topStories;
 	}
 
-	function buildSection({ category, topStories }) {
+	function buildSection({ section, topStories }) {
 		const articles = topStories
 			.map(
-				story => `
+				article => `
 				<article>
-          <h3><a href="${story.url}">${story.title}</a></h3>
-            <p>${story.byline}</p>
-            <p>${story.abstract}</p>
-            <img src="${story.multimedia[3].url}" alt="${story.multimedia[3].caption}"/>
+          <h3><a href="${article.url}">${article.title}</a></h3>
+            <p>${article.byline}</p>
+            <p>${article.abstract}</p>
+            <img src="${article.multimedia[3].url}" alt="${article.multimedia[3].caption}"/>
 				</article>
 				`
 			)
 			.join('');
-		const sectionTitle = category.toUpperCase();
+		const sectionTitle = section.toUpperCase();
 		const HTMLString = `<section><h2>${sectionTitle}</h2>${articles}</section>`;
 		return HTMLString;
 	}
@@ -38,14 +39,14 @@
 		return app;
 	}
 
-	categories.forEach(category =>
+	sections.forEach(section =>
 		fetch(
-			`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${apiKey}`
+			`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apiKey}`
 		)
 			.then(response => getJSON(response))
 			.then(({ results }) => results)
-			.then(articles => getTopStories(articles, 5))
-			.then(topStories => buildSection({ category, topStories }))
+			.then(articles => getTopStories(articles, numOfArticles))
+			.then(topStories => buildSection({ section, topStories }))
 			.then(HTMLString => renderApp(HTMLString))
 			.catch(error => console.log('Something went wrong:', error))
 	);
