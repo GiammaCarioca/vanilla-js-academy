a9459fdeea96f6cfa5a75deb0e3f3783d0536ca4
Thiago Giammattey <giammattey@gmail.com>
2019-11-05 11:45:29 -0300

    refactoring functions

--- pages/12-shuffling-arrays/js/main.js
+++ pages/12-shuffling-arrays/js/main.js
@@ -1,25 +1,3 @@
-/**
- * Randomly shuffle an array
- * https://stackoverflow.com/a/2450976/1293256
- * @param  {Array} array The array to shuffle
- * @return {String}      The first item in the shuffled array
- */
-var shuffle = function(array) {
-	var currentIndex = array.length;
-	var temporaryValue, randomIndex;
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		// And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-	return array;
-};
-
 // The monsters and socks
 const monsters = [
 	'monster1',
@@ -36,21 +14,45 @@ const monsters = [
 	'sock'
 ];
 
-const app = document.querySelector('#app');
 const grid = document.querySelector('.row');
 
-function createGridItems(monsters) {
-	shuffle(monsters);
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+const shuffle = function(array) {
+	let currentIndex = array.length;
+	let temporaryValue, randomIndex;
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+};
+
+function createCells(monsters) {
 	return monsters.map(
-		monster => `<img src="./assets/svg/${monster}.svg" alt="${monster}">`
+		monster => `<img src="assets/svg/${monster}.svg" alt="${monster}">`
 	);
 }
 
-function buildGrid(gridItems) {
-	return (grid.innerHTML = gridItems
+function buildGrid() {
+	const cells = createCells(monsters);
+	return shuffle(cells)
 		.map(item => `<div class="grid">${item}</div>`)
-		.join(''));
+		.join('');
 }
 
-const gridItems = createGridItems(monsters);
-buildGrid(gridItems);
+function renderGrid() {
+	grid.innerHTML = buildGrid();
+}
+
+renderGrid();
