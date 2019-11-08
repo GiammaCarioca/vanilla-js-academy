dc9633a93acb095a90bf3f27eb2faba092a6f831
Thiago Giammattey <giammattey@gmail.com>
2019-11-08 20:34:16 -0300

    minify css and js files

--- pages/12-shuffling-arrays/gulpfile.js
+++ pages/12-shuffling-arrays/gulpfile.js
@@ -1,16 +1,34 @@
-let gulp = require('gulp')
-let cleanCSS = require('gulp-clean-css')
+// Require the npm modules we need
+const gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	cleanCSS = require('gulp-clean-css'),
+	terser = require('gulp-terser')
 
-gulp.task('minify-css', () => {
+// Looks for a file called styles.css inside the css directory
+// Copies and renames the file to styles.min.css
+// Minifies the CSS
+// Saves the new file inside the css directory
+function minifyCSS() {
 	return gulp
-		.src('styles/*.css')
-		.pipe(cleanCSS({ compatibility: 'ie8' }))
-		.pipe(gulp.dest('dist'))
-})
-
-function defaultTask(cb) {
-	// place code for your default task here
-	cb()
+		.src('./css/styles.css')
+		.pipe(rename('styles.min.css'))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('./css'))
 }
 
-exports.default = defaultTask
+// Looks for a file called app.js inside the js directory
+// Copies and renames the file to app.min.js
+// Minifies the JS
+// Saves the new file inside the js directory
+function minifyJS() {
+	return gulp
+		.src('./js/app.js')
+		.pipe(rename('app.min.js'))
+		.pipe(terser())
+		.pipe(gulp.dest('./js'))
+}
+
+// Makes both these functions available as a single default task
+// The two functions will execute asynchronously (gulp.series() would be synchronous)
+// The task will run when you use the gulp command in the terminal
+exports.default = gulp.parallel(minifyCSS, minifyJS)
