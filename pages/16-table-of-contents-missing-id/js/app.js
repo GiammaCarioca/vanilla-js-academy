042875d68e33b173e438be397052c4548149413e
Thiago Giammattey <giammattey@gmail.com>
2019-11-13 18:22:05 -0300

    refactoring regex

--- pages/16-table-of-contents-missing-id/js/app.js
+++ pages/16-table-of-contents-missing-id/js/app.js
@@ -5,26 +5,19 @@
 	const headings = Array.prototype.slice.call(document.querySelectorAll('h2'))
 
 	function createID(heading) {
-		const id = heading.textContent
-			.replace(new RegExp("'", 'g'), ' ')
-			.replace(new RegExp(' ', 'g'), '-')
-			.toLowerCase()
-		return id
+		return heading.textContent.replace(/\W+/g, '-').toLowerCase()
 	}
 
 	if (headings.length > 0) {
-		headings.map(heading => {
-			if (heading.id) return
-			heading.id = createID(heading)
-		})
-
 		return (tableOfContents.innerHTML =
 			'<h2>Table of Contents</h2>' +
 			'<ol>' +
 			headings
 				.map(
 					heading =>
-						`<li><a href=#${heading.id}>${heading.textContent}</a></li>`
+						`<li><a href=#${heading.id || (heading.id = createID(heading))}>${
+							heading.textContent
+						}</a></li>`
 				)
 				.join('') +
 			'</ol>')
