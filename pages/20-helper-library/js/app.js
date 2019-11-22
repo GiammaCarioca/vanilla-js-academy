f5742d2aae217df35fa738bbf6f5da2a903e4807
Thiago Giammattey <giammattey@gmail.com>
2019-11-22 10:12:27 -0300

    refactoring

--- pages/20-helper-library/js/app.js
+++ pages/20-helper-library/js/app.js
@@ -1,58 +1,171 @@
-const tinyDOM = (function() {
+const cosa = (function() {
 	'use strict'
 
 	// Holds our public methods
 	const methods = {}
+
+	/**
+	 * Array.from() polyfill
+	 */
+	// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+	// Production steps of ECMA-262, Edition 6, 22.1.2.1
+	if (!Array.from) {
+		Array.from = (function() {
+			var toStr = Object.prototype.toString
+			var isCallable = function(fn) {
+				return (
+					typeof fn === 'function' || toStr.call(fn) === '[object Function]'
+				)
+			}
+			var toInteger = function(value) {
+				var number = Number(value)
+				if (isNaN(number)) {
+					return 0
+				}
+				if (number === 0 || !isFinite(number)) {
+					return number
+				}
+				return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number))
+			}
+			var maxSafeInteger = Math.pow(2, 53) - 1
+			var toLength = function(value) {
+				var len = toInteger(value)
+				return Math.min(Math.max(len, 0), maxSafeInteger)
+			}
+
+			// The length property of the from method is 1.
+			return function from(arrayLike /*, mapFn, thisArg */) {
+				// 1. Let C be the this value.
+				var C = this
+
+				// 2. Let items be ToObject(arrayLike).
+				var items = Object(arrayLike)
+
+				// 3. ReturnIfAbrupt(items).
+				if (arrayLike == null) {
+					throw new TypeError(
+						'Array.from requires an array-like object - not null or undefined'
+					)
+				}
+
+				// 4. If mapfn is undefined, then let mapping be false.
+				var mapFn = arguments.length > 1 ? arguments[1] : void undefined
+				var T
+				if (typeof mapFn !== 'undefined') {
+					// 5. else
+					// 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+					if (!isCallable(mapFn)) {
+						throw new TypeError(
+							'Array.from: when provided, the second argument must be a function'
+						)
+					}
+
+					// 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+					if (arguments.length > 2) {
+						T = arguments[2]
+					}
+				}
+
+				// 10. Let lenValue be Get(items, "length").
+				// 11. Let len be ToLength(lenValue).
+				var len = toLength(items.length)
+
+				// 13. If IsConstructor(C) is true, then
+				// 13. a. Let A be the result of calling the [[Construct]] internal method
+				// of C with an argument list containing the single item len.
+				// 14. a. Else, Let A be ArrayCreate(len).
+				var A = isCallable(C) ? Object(new C(len)) : new Array(len)
+
+				// 16. Let k be 0.
+				var k = 0
+				// 17. Repeat, while k < len… (also steps a - h)
+				var kValue
+				while (k < len) {
+					kValue = items[k]
+					if (mapFn) {
+						A[k] =
+							typeof T === 'undefined'
+								? mapFn(kValue, k)
+								: mapFn.call(T, kValue, k)
+					} else {
+						A[k] = kValue
+					}
+					k += 1
+				}
+				// 18. Let putStatus be Put(A, "length", len, true).
+				A.length = len
+				// 20. Return A.
+				return A
+			}
+		})()
+	}
 
 	//
 	// Methods
 	//
 
 	/**
-	 * Convert a NodeList to an array
-	 * @param  {NodeList}
-	 * @return {Array}
+	 * Convert an array-like object into an array
+	 * @param  {*}     list An array-like object (ex. NodeList)
+	 * @return {Array}      An array
 	 */
-	methods.makeArr = nodeList => Array.from(nodeList)
+	methods.toArray = list => Array.from(list)
 
 	/**
-	 * Get the first matching element in the DOM
-	 * @param  {NodeList}
-	 * @return
+	 * Get the first element that match a selector
+	 * @param  {String} selector The selector
+	 * @return {Node}            The matching element
 	 */
-	methods.getFirst = function(elements) {
-		return methods.makeArr(elements)[0]
-	}
+	methods.get = selector => document.querySelector(selector)
 
 	/**
-	 * Get all matching elements in the DOM as an array
-	 * @param  {NodeList}
-	 * @return {Array}
+	 * Get all elements that match a selector
+	 * @param  {String} selector The selector
+	 * @return {Array}           The matching elements
 	 */
-	methods.getElements = function(elements) {
-		return methods.makeArr(elements)
-	}
+	methods.getAll = selector =>
+		methods.toArray(document.querySelectorAll(selector))
 
 	/**
-	 * Add a class from all elements in an array
-	 * @param  {Array}  array
-	 * @param  {String} string
-	 * @return {Array}
+	 * Add a class to all elements in an array
+	 * @param  {Array}  elems     The elements
+	 * @param  {String} className The class to add
 	 */
-	methods.addClass = (arr, className) =>
-		arr.forEach(item => item.classList.add(className))
+	methods.addClass = (elems, className) =>
+		elems.forEach(elem => elem.classList.add(className))
 
 	/**
-	 * Remove a class from all elements in an array
-	 * @param  {Array}  array
-	 * @param  {String} string
-	 * @return {Array}
+	 * Remove a class to all elements in an array
+	 * @param  {Array}  elems     The elements
+	 * @param  {String} className The class to remove
 	 */
-	methods.removeClass = (arr, className) =>
-		arr.forEach(item => item.classList.remove(className))
+	methods.removeClass = (elems, className) =>
+		elems.forEach(elem => elem.classList.remove(className))
 
 	//
-	// Return public methods
+	// Return the public methods
 	//
 	return methods
 })()
+
+/**
+ * TESTING
+ */
+
+// cosa.toArray()
+const arr = cosa.toArray(document.querySelectorAll('button'))
+console.log('cosa.toArray()', arr)
+
+// cosa.get()
+var btn = cosa.get('#button-2')
+console.log('cosa.get()', btn)
+
+// cosa.getAll()
+var btns = cosa.getAll('button')
+console.log('cosa.getAll()', btns)
+
+// cosa.addClass()
+cosa.addClass(btns, 'btn-purple')
+
+// cosa.removeClass()
+cosa.removeClass(btns, 'btn-blue')
