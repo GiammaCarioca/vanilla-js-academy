4d7f25ce27707d7310a7c0c4132e55ba4b99a36b
Thiago Giammattey <giammattey@gmail.com>
2019-11-29 09:42:26 -0300

    add comments + refactoring

--- pages/24-autosave/js/app.js
+++ pages/24-autosave/js/app.js
@@ -1,28 +1,54 @@
 ;(function() {
 	'use strict'
 
+	//
+	// Variables
+	//
 	const form = document.querySelector('#save-me')
 
-	const saveFormData = evt => {
-		if (!evt.target.matches('input')) return
+	//
+	// Methods
+	//
+
+	/**
+	 * Handle input events
+	 * @param  {Event} event The event object
+	 */
+	const inputHandler = evt => {
+		if (!event.target.closest('#save-me')) return
 		localStorage.setItem(evt.target['name'], evt.target.value)
 	}
 
-	const handleSubmit = evt => {
+	/**
+	 * Handle submit events
+	 * @param  {Event} event The event object
+	 */
+	const submitHandler = evt => {
 		if (!evt.target.closest('form').matches('#save-me')) return
 		localStorage.clear()
 	}
 
+	/**
+	 * Load saved form data from localStorage
+	 */
 	const populateForm = _ => {
 		for (let item in localStorage) {
 			if (localStorage.hasOwnProperty(item)) {
-				form.children[item].value = localStorage.getItem(item)
+				form[item].value = localStorage.getItem(item)
 			}
 		}
 	}
 
+	//
+	// Inits & Event Listeners
+	//
+
+	// Load saved data from storage
 	populateForm()
 
-	form.addEventListener('input', saveFormData, false)
-	form.addEventListener('submit', handleSubmit, false)
+	// Listen for input events
+	document.addEventListener('input', inputHandler, false)
+
+	// Listen for submit events
+	document.addEventListener('submit', submitHandler, false)
 })()
