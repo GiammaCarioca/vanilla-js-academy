297d48de7194cca04240c4a9a9940107c778f8f3
Thiago Giammattey <giammattey@gmail.com>
2019-12-10 13:40:05 -0300

    cleaning it up

--- pages/29-countdown-timer/js/app.js
+++ pages/29-countdown-timer/js/app.js
@@ -4,7 +4,7 @@
 	//
 	// Variables
 	//
-	const btn = document.querySelector('button')
+	const app = document.querySelector('#app')
 
 	const data = {
 		timer: 60
@@ -16,44 +16,41 @@
 	// Methods
 	//
 	const template = _ => {
-		if (count <= 0) {
-			btn.style.display = 'inline-block'
-			return '<p>⏰</p>'
+		if (count === 0) {
+			return '<p>⏰</p><button id="restart">Restart Timer</button>'
 		}
 
 		return `<p>${count}</p>`
 	}
 
 	const render = _ => {
-		const display = document.querySelector('#display')
-		if (!display) return
-		display.innerHTML = template()
+		app.innerHTML = template()
 	}
 
 	const startTimer = _ => {
 		const countdown = setInterval(function() {
-			count--
-
 			render()
 
-			if (count <= 0) {
+			count--
+
+			if (count < 0) {
 				clearInterval(countdown)
 			}
 		}, 1000)
 	}
 
-	const restart = _ => {
-		count = data.timer
-		btn.style.display = 'none'
-		render()
-		startTimer()
+	const restartTimer = _ => {
+		if (event.target.id === 'restart') {
+			count = data.timer
+
+			startTimer()
+		}
 	}
 
 	//
 	// Inits & Event Listeners
 	//
-	render()
 	startTimer()
 
-	btn.addEventListener('click', restart, false)
+	document.addEventListener('click', restartTimer, false)
 })()
