1ab1d5c601ebe272d5f91cf60cf2b0f8f41c4d1f
Thiago Giammattey <giammattey@gmail.com>
2019-12-10 10:13:30 -0300

    add countdown timer

--- pages/29-countdown-timer/js/app.js
+++ pages/29-countdown-timer/js/app.js
@@ -1,4 +1,55 @@
 ;(function() {
 	'use strict'
 
+	//
+	// Variables
+	//
+	const btn = document.querySelector('button')
+
+	let timer = 10
+
+	//
+	// Methods
+	//
+	const template = _ => {
+		if (timer <= 0) {
+			btn.style.display = 'inline-block'
+			return '<p>⏰</p>'
+		}
+
+		return `<p>${timer}</p>`
+	}
+
+	const render = _ => {
+		const display = document.querySelector('#display')
+		if (!display) return
+		display.innerHTML = template()
+	}
+
+	const startTimer = _ => {
+		const countdown = setInterval(function() {
+			timer--
+
+			render()
+
+			if (timer <= 0) {
+				clearInterval(countdown)
+			}
+		}, 1000)
+	}
+
+	const restart = _ => {
+		timer = 10
+		btn.style.display = 'none'
+		render()
+		startTimer()
+	}
+
+	//
+	// Inits & Event Listeners
+	//
+	render()
+	startTimer()
+
+	btn.addEventListener('click', restart, false)
 })()
