bbbb6bafcd4f758560fa3cc73185ed16abf16a43
Thiago Giammattey <giammattey@gmail.com>
2019-12-11 08:54:20 -0300

    add formatted time

--- pages/30-timer-formatted/js/app.js
+++ pages/30-timer-formatted/js/app.js
@@ -7,7 +7,7 @@
 	const app = document.querySelector('#app')
 
 	const data = {
-		timer: 60
+		timer: 120
 	}
 
 	let count = data.timer
@@ -15,12 +15,44 @@
 	//
 	// Methods
 	//
+
+	/**
+	 * String.prototype.padStart() polyfill
+	 * https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+	 */
+	if (!String.prototype.padStart) {
+		String.prototype.padStart = function padStart(targetLength, padString) {
+			targetLength = targetLength >> 0 //truncate if number or convert non-number to 0;
+			padString = String(typeof padString !== 'undefined' ? padString : ' ')
+			if (this.length > targetLength) {
+				return String(this)
+			} else {
+				targetLength = targetLength - this.length
+				if (targetLength > padString.length) {
+					padString += padString.repeat(targetLength / padString.length) //append to original to ensure we are longer than needed
+				}
+				return padString.slice(0, targetLength) + String(this)
+			}
+		}
+	}
+
+	const formatTimer = time => {
+		const minutes = parseInt(time / 60, 10)
+		const seconds = parseInt(time - minutes * 60, 10)
+
+		const paddedMinutes = minutes.toString().padStart(2, '0')
+		const paddedSeconds = seconds.toString().padStart(2, '0')
+
+		return `${paddedMinutes}:${paddedSeconds}`
+	}
+
 	const template = _ => {
 		if (count === 0) {
 			return '<p>⏰</p><button id="restart">Restart Timer</button>'
 		}
 
-		return `<p>${count}</p>`
+		return `<p>${formatTimer(count)}</p>`
 	}
 
 	const render = _ => {
