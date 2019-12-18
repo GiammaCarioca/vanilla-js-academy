f2625dff074ac3be6f36f25ea4bfd06691f76530
Thiago Giammattey <giammattey@gmail.com>
2019-12-18 14:11:26 -0300

    add some new styles and better comments

--- pages/33-todo-list/js/app.js
+++ pages/33-todo-list/js/app.js
@@ -4,24 +4,38 @@
 	//
 	// Variables
 	//
-	const todo = document.querySelector('#new-todo')
+
+	// The new todo input field
+	const newTodo = document.querySelector('#new-todo')
 
 	//
 	// Methods
 	//
+
+	/**
+	 * Create a todo component
+	 */
 	const app = new Reef('#app', {
 		data: {
 			todos: []
 		},
 		template: function({ todos }) {
+			// If there are no todos, ask the user to add some
+			if (todos.length < 1) {
+				return "<p>You don't have any todos yet. Add some using the form above.</p>"
+			}
+
+			// Generate markup for todo items
 			return (
-				'<ul>' +
+				'<ul class="todos">' +
 				todos
 					.map(
 						(todo, index) =>
-							`<li><input id=${index} type="checkbox" ${
+							`<li ${
+								todo.completed ? 'class="todo-completed"' : ''
+							}><label for='${index}'><input type="checkbox" id="todo-${index}" data-todo="${index}" ${
 								todo.completed ? 'checked' : ''
-							}><label for='${index}'>${todo.text}</label></li>`
+							}>${todo.text}</label></li>`
 					)
 					.join('') +
 				'</ul>'
@@ -29,34 +43,67 @@
 		}
 	})
 
-	const changeHandler = function(e) {
-		if (e.target.matches('[type="checkbox"]')) {
-			const { id, checked } = e.target
-			;(app.data.todos = [...app.data.todos]),
-				(app.data.todos[id].completed = checked)
-			console.log(app.data.todos[id])
-		}
-		app.render()
-	}
-
 	const addTodo = function(todo) {
-		if (!todo.value) return
+		// Get a copy of the todos and update data object
+		const todos = [...app.data.todos, { text: todo.value, completed: false }]
 
-		app.data.todos = [...app.data.todos, { text: todo.value, completed: false }]
+		// Render fresh UI
+		app.setData({ todos: todos })
+
+		// Clear the field and return focus
 		todo.value = ''
+		todo.focus()
+	}
+
+	/**
+	 * Handle form submit events
+	 * @param  {Event} event The Event object
+	 */
+	const submitHandler = function(e) {
+		// Only run for #add-todos form
+		if (e.target.id !== 'add-todos') return
+
+		// Stop the form from reloading the page
+		e.preventDefault()
+
+		// If the #new-todo input has no value, do nothing
+		if (newTodo.value.length < 1) return
+
+		addTodo(newTodo)
+	}
+
+	/**
+	 * Handle click events
+	 * @param  {Event} event The Event object
+	 */
+	const clickHandler = function(e) {
+		// Only run on todo items
+		const todo = event.target.getAttribute('data-todo')
+		if (!todo) return
+
+		// Get a copy of the todos
+		const todos = [...app.data.todos]
+		if (!todos[todo]) return
+
+		// Update the todo state
+		todos[todo].completed = e.target.checked
+
+		console.log(todos[todo])
+
+		// Render fresh UI
 		app.render()
 	}
 
-	const submitHandler = function(e) {
-		if (e.target.matches('#add-todos')) {
-			e.preventDefault()
+	//
+	// Inits & Event Listeners
+	//
 
-			addTodo(todo)
-		}
-	}
-
-	document.addEventListener('submit', submitHandler, false)
-	document.addEventListener('change', changeHandler, false)
-
+	// Render the initial UI
 	app.render()
+
+	// Listen for form submit events
+	document.addEventListener('submit', submitHandler, false)
+
+	// Listen for click events
+	document.addEventListener('click', clickHandler, false)
 })()
